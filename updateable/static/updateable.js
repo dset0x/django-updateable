c34bf70f99a9be47be494cb7bac1bae783cd7d09
Baldur Þór Emilsson <baldur@baldur.biz>
2013-05-05 14:08:16 +0000

    JS refactoring, not using jQuery now

--- updateable/static/updateable.js
+++ updateable/static/updateable.js
@@ -1,34 +1,59 @@
-var updateable = function(options) {
-  var settings = $.extend({
-    timeout: 3000,
-    callback: function() {},
-    getVariable: 'update'
-  }, options);
-
-  var update = function() {
-    var ids = [], hash = [];
-    $('[data-updateable]').each(function() {
-      ids.push($(this).data('updateable'));
-      hash.push($(this).data('hash'));
-    });
-    var getVars = {
-      ids: ids,
-      hash: hash
-    };
-    getVars[settings.getVariable] = true;
-    $.get('', getVars, function(data) {
-      var updated = $(data);
-      $('[data-updateable]').each(function() {
-        var id = $(this).data('updateable');
-        var ud = updated.find('[data-updateable="' + id + '"]');
-        if(ud.length == 1) {
-          $(this).replaceWith(ud[0]);
-          settings.callback.call(ud[0]);
-        }
-      });
-      setTimeout(update, settings.timeout);
-    });
+(function() {
+  var us = window.updateableSettings || {};
+  var settings = {
+    timeout: us.timeout || 3000,
+    callback: us.callback || function() {},
+    getVariable: us.getVariable || 'update'
   };
 
-  setTimeout(update, settings.timeout);
-};
+
+  var getUpdateables = function() {
+    return document.querySelectorAll('[data-updateable]');
+  };
+
+  var readyStateChanged = function() {
+    if(this.readyState != 4)
+        return;
+    if(this.status == 200) {
+      var fragment = document.createDocumentFragment();
+      var el = document.createElement('div');
+      el.innerHTML = this.responseText;
+      fragment.appendChild(el);
+
+      var updateables = getUpdateables();
+      for(var i = 0; i < updateables.length; i++) {
+        var updateable = updateables[i];
+        var id = updateable.getAttribute('data-updateable');
+        var updated = fragment.querySelector('[data-updateable=' + id + ']');
+        if(updated) {
+          updateable.parentNode.replaceChild(updated, updateable);
+          settings.callback.call(updated);
+        }
+      }
+    }
+    setTimeout(update, settings.timeout)
+  };
+
+  var update = function() {
+    var url = '?' + encodeURI(settings.getVariable + '=true');
+    var updateables = getUpdateables();
+    for(var i = 0; i < updateables.length; i++) {
+      var updateable = updateables[i];
+      var id = updateable.getAttribute('data-updateable');
+      var hash = updateable.getAttribute('data-hash');
+      url += encodeURI('&id[]=' + id);
+      url += encodeURI('&hash[]=' + hash);
+    }
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = readyStateChanged;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  };
+
+  if(document.readyState === 'complete')
+    setTimeout(update);
+  else if(document.addEventListener)
+    document.addEventListener('DOMContentLoaded', update);
+  else
+    window.attachEvent('onload', update);
+})();
