65e94ce08db53e59e6508f5f63a46494edb8cf0b
Baldur Þór Emilsson <baldur@baldur.biz>
2013-05-06 00:09:24 +0000

    Version 0.2 in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='django-updateable',
-    version='0.1',
+    version='0.2',
     author=u'Baldur Þór Emilsson',
     author_email='baldur@baldur.biz',
     packages=['updateable', 'updateable.templatetags'],
