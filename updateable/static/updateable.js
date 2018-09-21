4b7decd47db93ef441fd854cdb739df72ba76a26
Dimitrios Semitsoglou-Tsiapos <kmhzsem@gmx.com>
2018-09-21 10:50:23 +0200

    Fix support for multiple updateables
    Parse updated as XML so as to be able to reach for elements regardless
    of their type.

--- updateable/static/updateable.js
+++ updateable/static/updateable.js
@@ -20,22 +20,33 @@ window.updateableSettings = (function() {
     if(this.readyState != 4)
       return;
     if(this.status == 200 && settings.autoUpdate) {
-      var fragment = document.createDocumentFragment();
-      var el = document.createElement('template');
-      el.innerHTML = this.responseText;
-      fragment.appendChild(el);
+      const parser = new DOMParser();
+      if (/\S/.test(this.responseText)) {
 
-      var updateables = getUpdateables();
-      for(var i = 0; i < updateables.length; i++) {
-        var updateable = updateables[i];
-        var id = updateable.getAttribute('data-updateable');
-        var updated = fragment.firstChild.content.querySelector('[data-updateable="' + id + '"]');
-        if(updated) {
-          updateable.parentNode.replaceChild(updated, updateable);
-          settings.callback.call(updated);
-        }
+        // Get children from response
+        var _fragment = document.createDocumentFragment();
+        var _el = document.createElement('template');
+        _el.innerHTML = this.responseText;
+        _fragment.appendChild(_el);
+        var children = _fragment.firstChild.content.children; // FIXME: might fail
+
+        var updateables = getUpdateables();
+        for(var i = 0; i < updateables.length; i++) {
+          var updateable = updateables[i];
+          var id_to_search = updateable.getAttribute('data-updateable');
+
+          for (j=0; j<children.length; ++j) {
+            const updated = children[j].content.querySelector('[data-updateable="' + id_to_search + '"]')
+            if (updated) {
+              updateable.parentNode.replaceChild(updated, updateable);
+            } // if updated
+
+          } // for children
+        } // for updateables
+
       }
     }
+    settings.callback.call();
     retry();
   };
 
--- updateable/templatetags/updateable.py
+++ updateable/templatetags/updateable.py
@@ -3,56 +3,47 @@
 from hashlib import md5
 from django.template import Library, Node, TemplateSyntaxError
 from .. import settings
+import logging
 
 
 register = Library()
 
 class UpdateableNode(Node):
-    def __init__(self, element, nodelist):
+    def __init__(self, element_id, element, nodelist):
         self.element = element
+        self.element_id = element_id
         self.nodelist = nodelist
 
     def render(self, context):
         request = context.get('request')
-        updateable_count = context.render_context.get('updateable_count', 1)
-        context.render_context['updateable_count'] = updateable_count + 1
-
-        id = self.get_id(request, updateable_count)
         contents = u''.join(n.render(context) for n in self.nodelist)
         hash = md5(contents.encode()).hexdigest()
         rcontext = {
-            'id': id,
+            'id': self.element_id,
             'contents': contents,
             'hash': hash,
             'element': self.element,
         }
         output = u'<%(element)s data-updateable="%(id)s" data-hash="%(hash)s">%(contents)s</%(element)s>' % rcontext
 
+        if bool(request.GET.get(settings.UPDATEABLE_GET_VARIABLE)):
+            output = f'<template>{output}</template>'
         updateable_dict = getattr(request, settings.UPDATEABLE_REQUEST_OBJECT)
         if updateable_dict['updateable'] and updateable_dict['hashes'].get(id, '') != hash:
             updateable_dict['contents'].append(output)
         return output
 
-    def get_id(self, request, updateable_count):
-        if not request:
-            id = updateable_count
-        else:
-            decoded = '%d-%s' % (updateable_count, request.path)
-            id = md5(decoded.encode()).hexdigest()
-        return id
-
 
 @register.tag
 def updateable(parser, token):
     contents = token.split_contents()
-    if len(contents) == 1:
-        element = 'div'
-    elif len(contents) == 2:
-            element = contents[1]
+    try:
+        element_id = contents[1]
+        element = contents[2]
+    except IndexError:
+        raise TemplateSyntaxError('%s must have at least 1 argument' % contents[0])
     else:
-        raise TemplateSyntaxError('%s can have at most one argument' % contents[0])
-
-    nodelist = parser.parse(('endupdateable',))
-    parser.delete_first_token()
-    return UpdateableNode(element, nodelist)
+        nodelist = parser.parse(('endupdateable',))
+        parser.delete_first_token()
+        return UpdateableNode(element_id, element, nodelist)
 
